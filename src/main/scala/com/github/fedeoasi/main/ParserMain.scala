3526bc1cc2ef7cedf735fc52f4d729dc6d53f9e9
Federico Caimi <fedeoasi@gmail.com>
2015-09-20 09:17:43 -0500

    More console formatting.

--- src/main/scala/com/github/fedeoasi/main/ParserMain.scala
+++ src/main/scala/com/github/fedeoasi/main/ParserMain.scala
@@ -52,27 +52,27 @@ object ParserMain {
     }
     prettyPrint(scoredStationDays, "Station Days")
 
-    println(s"Computing Top $K Busiest Days")
+    println(s"Computing Top $K Busiest Days...")
     val ridesByDay = dailyRideCounts.groupBy(_.date)
     val days = rankGroupedRideCounts(ridesByDay)
     prettyPrint(days, s"Busiest Days")
 
-    println(s"Computing Top $K Busiest Years")
+    println(s"Computing Top $K Busiest Years...")
     val ridesByYear = dailyRideCounts.groupBy(_.date.getYear)
     val years = rankGroupedRideCounts(ridesByYear)
     prettyPrint(years, s"Busiest Years")
 
-    println(s"Computing Top $K Busiest stations of all time")
+    println(s"Computing Top $K Busiest stations of all time...")
     val ridesByStation = dailyRideCounts.groupBy(_.station)
     val busiestStations = rankGroupedRideCounts(ridesByStation)
     prettyPrint(busiestStations, s"Busiest Stations")
 
-    println(s"Computing Top $K Busiest day types")
+    println(s"Computing Top $K Busiest day types...")
     val ridesByDayType = dailyRideCounts.groupBy(_.dayType)
     val busiestDayTypes = rankGroupedRideCounts(ridesByDayType)
     prettyPrint(busiestDayTypes, s"Busiest Day Type")
 
-    println(s"Computing Top $K Train Lines")
+    println(s"Computing Top $K Train Lines...")
     val ridesAndStations = computeRidesAndStations(stations, dailyRideCounts)
     val lineAndRidesSeq = ridesAndStations.flatMap { ridesAndStation =>
       ridesAndStation.station.lines.map { line =>
@@ -97,7 +97,9 @@ object ParserMain {
 
   def prettyPrint(results: Seq[ScoredResult[_]], title: String): Unit = {
     val colors = Seq(GREEN, YELLOW, RED)
-    println(s"Results for $title")
+    printSeparator()
+    println(s"${CYAN}Results for $title$RESET")
+    printSeparator()
     results.zipWithIndex.foreach { case (r, i) =>
       val highlight = if (i < colors.size) colors(i) else ""
       println(s"$highlight${NumberFormat.getInstance().format(r.score)}\t${r.value}$RESET")
